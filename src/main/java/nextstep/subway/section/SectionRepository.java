829cffeb17ebbddcd7de1e575c362fdc4954e668
Lee seung chan <dltmdcks702@gmail.com>
2024-02-03 09:12:16 +0900

    fix: adapt change domain entity on repository

--- src/main/java/nextstep/subway/section/SectionRepository.java
+++ src/main/java/nextstep/subway/section/SectionRepository.java
@@ -4,8 +4,4 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
-
-    int countByLineId(Long lineId);
-
-    Optional<Section> findByLineIdAndDownStationId(Long lineId, Long downStationId);
 }
