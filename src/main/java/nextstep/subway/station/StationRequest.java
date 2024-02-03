3bb020881d9fea662ca4f80cfbcd75ee6098ff5b
Lee seung chan <dltmdcks702@gmail.com>
2024-02-03 09:21:34 +0900

    feat: add constructor to do the test

--- src/main/java/nextstep/subway/station/StationRequest.java
+++ src/main/java/nextstep/subway/station/StationRequest.java
@@ -3,6 +3,13 @@ package nextstep.subway.station;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {
+    }
+
+    public StationRequest(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
